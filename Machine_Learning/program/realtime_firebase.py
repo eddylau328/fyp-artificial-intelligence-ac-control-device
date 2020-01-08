9c5585bd68012d293f5eb2e4c90325ed2225b9dc
Eddy Lau <eddylau0328@gmail.com>
2020-01-08 14:35:21 +0800

    installed firebase-admin and added realtime_firebase.py

---
+++ Machine_Learning/program/realtime_firebase.py
@@ -0,0 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+class Realtime_firebase:
+
+    def __init__(self):
+        # Fetch the service account key JSON file contents
+        cred = credentials.Certificate('../../../datalog-418c9-firebase-adminsdk-og0bw-bacec2a5fc.json')
+        # Initialize the app with a service account, granting admin privileges
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://datalog-418c9.firebaseio.com/'
+        })
+
+    def getLatestData(self):
+        ref = db.reference('sensor')
+        # forming a list of data
+        data = [detail for _ , detail in ref.get().get('data').items()]
+        # get the latest data from the list
+        return data.pop()
+
+    def sendAction(self, action):
+        ref = db.reference('step')
+        ref.push({'action':action})
+
+firebase = Realtime_firebase()
+print(firebase.getLatestData())
+firebase.sendAction(0)
