11a6eeb0f76c31e7e4f5fcdb1a706d6f953c9a62
Eddy <eddylau0328@gmail.com>
2020-01-11 13:08:43 +0800

    Added a button to go to login activity

--- Mobile_app/FYP_AC_Monitor/app/src/main/java/com/example/fyp_ac_monitor/MainActivity.java
+++ Mobile_app/FYP_AC_Monitor/app/src/main/java/com/example/fyp_ac_monitor/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.fyp_ac_monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button _startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _startButton = (Button) findViewById((R.id.button));
+        _startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Can Add if else case to go to login or by-pass the login
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
