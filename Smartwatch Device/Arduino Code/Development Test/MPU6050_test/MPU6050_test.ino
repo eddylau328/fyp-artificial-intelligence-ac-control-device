d31749caa5f11a694289f7d50cbf28d363f81c90
Eddy Lau <eddylau0328@gmail.com>
2020-01-25 02:56:53 +0800

    added class page, class page_monitor, class oled

--- Smartwatch Device/Arduino Code/Development Test/MPU6050_test/MPU6050_test.ino
+++ Smartwatch Device/Arduino Code/Development Test/MPU6050_test/MPU6050_test.ino
@@ -100,7 +100,7 @@ MPU6050 mpu;
 // from the FIFO. Note this also requires gravity vector calculations.
 // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
 // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_YAWPITCHROLL
 
 // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
 // components with gravity removed. This acceleration reference frame is
@@ -287,10 +287,10 @@ void loop() {
 
     // get current FIFO count
     fifoCount = mpu.getFIFOCount();
-  if(fifoCount < packetSize){
-          //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
-      // This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-  }
+    if(fifoCount < packetSize){
+            //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+        // This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    }
     // check for overflow (this should never happen unless our code is too inefficient)
     else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
         // reset so we can continue cleanly
@@ -302,96 +302,41 @@ void loop() {
     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
 
         // read a packet from FIFO
-  while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-  }
-        #ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print("aworld\t");
-            Serial.print(aaWorld.x);
-            Serial.print("\t");
-            Serial.print(aaWorld.y);
-            Serial.print("\t");
-            Serial.println(aaWorld.z);
-        #endif
-    
-        #ifdef OUTPUT_TEAPOT
-            // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-        #endif
-
+    while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+    }
+       read_acceleration();
         // blink LED to indicate activity
         blinkState = !blinkState;
         digitalWrite(LED_PIN, blinkState);
     }
+}
+
+void read_acceleration(){
+  // display real acceleration, adjusted to remove gravity
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetAccel(&aa, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  Serial.print("areal\t");
+  Serial.print(aaReal.x);
+  Serial.print("\t");
+  Serial.print(aaReal.y);
+  Serial.print("\t");
+  Serial.println(aaReal.z);
+}
+
+void read_euler(){
+  // display Euler angles in degrees
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetEuler(euler, &q);
+  Serial.print("euler\t");
+  Serial.print(euler[0] * 180/M_PI);
+  Serial.print("\t");
+  Serial.print(euler[1] * 180/M_PI);
+  Serial.print("\t");
+  Serial.println(euler[2] * 180/M_PI);
 }
--- Smartwatch Device/Arduino Code/Smartwatch_without_battery/Smartwatch_without_battery.ino
+++ Smartwatch Device/Arduino Code/Smartwatch_without_battery/Smartwatch_without_battery.ino
@@ -1,11 +1,18 @@
 #include <Wire.h>
 #include "MAX30105.h"
 
+#include <MPU6050_tockn.h>
+
 #include "heartRate.h"
 
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+#include <Timer.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -153,95 +160,347 @@ long lastBeat = 0; //Time at which the last beat occurred
 float beatsPerMinute;
 int beatAvg;
 
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 32
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature bodyTempSensor(&oneWire);
+/********************************************************************/ 
+float bodyTemp;
+
+int acc[3] = {0,0,0};
+int gyr[3] = {0,0,0};
+
+Timer tempTimer;
+Timer mpu6050Timer;
+
+MPU6050 mpu6050(Wire);
+
+// OneWire DS18S20, DS18B20, DS1822 Temperature Example
+//
+// http://www.pjrc.com/teensy/td_libs_OneWire.html
+//
+// The DallasTemperature library can do all this work for you!
+// https://github.com/milesburton/Arduino-Temperature-Control-Library
+
+OneWire  ds(32);  // on pin 10 (a 4.7K resistor is necessary)
+
+#define INITIAL_PAGE 0
+#define MAIN_PAGE 1
+#define TEMP_PAGE 2
+#define MOVE_PAGE 3
+#define MENU_PAGE 4
+
+class OLED{
+  public:
+    Adafruit_SSD1306 *oled;
+    
+    OLED(Adafruit_SSD1306 *oled){
+      this->oled = oled;
+      // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+      if(!oled->begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+      }
+    
+      // Show initial display buffer contents on the screen --
+      // the library initializes this with an Adafruit splash screen.
+      oled->display();
+      delay(2000); // Pause for 2 seconds
+      clear();
+    }
+
+    void display_logo() {
+      oled->drawBitmap(
+      (oled->width()  - LOGO_WIDTH ) / 2,
+      (oled->height() - LOGO_HEIGHT) / 2,
+      logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+      oled->display();
+    }
+
+    void display_text(String text, int x, int y, bool isClear) {
+      if (isClear)
+        clear();
+    
+      oled->setTextSize(1);             // Normal 1:1 pixel scale
+      oled->setTextColor(SSD1306_WHITE);        // Draw white text
+      oled->setCursor(x,y);             // Start at top-left corner
+      oled->println(text);
+      oled->display();
+    }
+    
+    void display_text(String text, int x, int y, bool isClear, int text_size) {
+      if (isClear)
+        clear();
+    
+      oled->setTextSize(text_size);             // Normal 1:1 pixel scale
+      oled->setTextColor(SSD1306_WHITE);        // Draw white text
+      oled->setCursor(x,y);             // Start at top-left corner
+      oled->println(text);
+      oled->display();
+    }
+    
+    void display_text_pic() {
+      oled->drawBitmap(
+        (oled->width()  -  TEXT_WIDTH) / 2,
+        (oled->height() - TEXT_HEIGHT) / 2,
+        displayText, TEXT_WIDTH, TEXT_HEIGHT, 1);
+      oled->display();
+    }
+    
+    void clear_text(String text, int x, int y, int text_size){
+      oled->setTextSize(text_size);             // Normal 1:1 pixel scale
+      oled->setTextColor(SSD1306_BLACK);        // Draw white text
+      oled->setCursor(x,y);             // Start at top-left corner
+      oled->println(text);
+      oled->display();
+    }
+
+    void draw_pixel(int x, int y){
+        // Draw a single pixel in white
+        oled->drawPixel(x, y, SSD1306_WHITE);
+        // Show the display buffer on the screen. You MUST call display() after
+        // drawing commands to make them visible on screen!
+        oled->display();
+    }
+
+    void clear(int x, int y){
+      oled->drawPixel(x, y, SSD1306_BLACK);
+    }
+    
+    void clear(){
+      oled->clearDisplay();
+    }
+    
+};
+
+class Page{
+  public:
+    int page_num;
+        
+    virtual void show(OLED oled){}
+    virtual void set_page_num(int page_num){};
+};
+
+class InitialPage: public Page{
+  public:
+
+    void show(OLED oled){
+      for (int i = 0; i < 6; i++){
+        oled.draw_pixel(10+i*5, 32);
+        delay(500);
+      }
+      oled.clear();
+      
+      oled.display_text("Welcome back, Eddy!", 0, 32, true);
+      delay(2000);
+      
+      oled.clear();
+      oled.display_text_pic();
+      delay(3000);
+      
+      oled.clear();
+      oled.display_logo();
+      delay(3000);
+      
+      oled.clear();
+    }
+
+    void set_page_num(int page_num){
+      this->page_num = page_num;
+    }
+};
+
+class PageMonitor{
+  private:
+    Page *pages;
+    int current_page = -1;
+    int total_page = 0;
+    OLED *oled;
+    
+  public:
+    void set(OLED *oled, Page *pages, int total_page){
+      this->oled = oled;
+      this->pages = pages;
+      this->total_page = total_page;
+      current_page = 0;
+    }
+
+    void show(int page_num){
+      switch (page_num){
+        case 0:
+          pages[0].show(*oled);
+          break;
+      }
+    }
+};
+
 void heartRateSensorSetup(){
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
 
   particleSensor.setup(); //Configure sensor with default settings
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED  
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green
 }
 
 void oledDisplaySetup(){
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-
-  // Clear the buffer
-  display.clearDisplay();
-
-  for (int i = 0; i < 6; i++){
-    // Draw a single pixel in white
-    display.drawPixel(10+i*5, 30, SSD1306_WHITE);
-    // Show the display buffer on the screen. You MUST call display() after
-    // drawing commands to make them visible on screen!
-    display.display();
-    delay(500);
-  }
-  display.clearDisplay();
-  oledDisplayText("Welcome back, Eddy!", 0, 32);
-  delay(2000);
-  oledDisplayTextPic();
-  delay(3000);
-  oledDisplayLogo();
+  //oledDisplayText("Eddy FYP", 0, 0, false,2);
+  //oledDisplayText("BMP: ", 0, 22, false, 1);
+  //oledDisplayText("TMP: ", 0, 32, false, 1);
+  //oledDisplayText("Ang: ", 0, 42, false, 1);
+  //oledDisplayText("Acc: ", 0, 52, false, 1);
 }
 
-void oledDisplayText(String text, int x, int y) {
-  display.clearDisplay();
-
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(x,y);             // Start at top-left corner
-  display.println(text);
-  display.display();
-}
-
-void oledDisplayLogo() {
-  display.clearDisplay();
-  display.drawBitmap(
-    (display.width()  - LOGO_WIDTH ) / 2,
-    (display.height() - LOGO_HEIGHT) / 2,
-    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
-  display.display();
-}
-
-void oledDisplayTextPic() {
-  display.clearDisplay();
-  display.drawBitmap(
-    (display.width()  -  TEXT_WIDTH) / 2,
-    (display.height() - TEXT_HEIGHT) / 2,
-    displayText, TEXT_WIDTH, TEXT_HEIGHT, 1);
-  display.display();
-}
+PageMonitor page_monitor;
+Page *page;
+InitialPage initial_page;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Initializing...");
+
+  page = &initial_page;
+
+  page_monitor.set(new OLED(&display), page, 1);
+
+  page_monitor.show(INITIAL_PAGE);
   
-  oledDisplaySetup();
-  //heartRateSensorSetup();
+  heartRateSensorSetup();
+
+  Wire.begin();
+  mpu6050.begin();
+
+// 21:47:15.290 -> X : 1.63
+// 21:47:15.290 -> Y : 0.43
+// 21:47:15.290 -> Z : -2.67
+
+  mpu6050.calcGyroOffsets(1.63,0.43,-2.67);
+
+  bodyTempSensor.begin(); 
+  
+  tempTimer.settimer(2000);
+  tempTimer.starttimer();
+
+  mpu6050Timer.settimer(1000);
+  mpu6050Timer.starttimer();  
+}
+
+String lastStrBeatAvg = "";
+String strBeatAvg;
+String lastStrTemp = "";
+String strTemp;
+
+String strAcc;
+String strGyr;
+String laststrAcc;
+String laststrGyr;
+/*
+void oledDisplayLoop(){
+  if (lastStrBeatAvg != strBeatAvg){
+    oledClearText(lastStrBeatAvg,30, 22, 1);
+    oledDisplayText(strBeatAvg, 30, 22, false, 1);
+  }
+  lastStrBeatAvg = strBeatAvg;
+
+  if (lastStrTemp != strTemp){
+    oledClearText(lastStrTemp,30, 32, 1);
+    oledDisplayText(strTemp, 30, 32, false, 1);    
+  }
+  lastStrTemp = strTemp;
+
+  if (laststrGyr != strGyr){
+    oledClearText(laststrGyr,30, 42, 1);
+    oledDisplayText(strGyr, 30, 42, false, 1);    
+  }
+  laststrGyr = strGyr;
+
+  if (laststrAcc != strAcc){
+    oledClearText(laststrAcc,30, 52, 1);
+    oledDisplayText(strAcc, 30, 52, false, 1);    
+  }
+  laststrAcc = strAcc;
+  
+}
+*/
+void readTemperature(){
+   // call sensors.requestTemperatures() to issue a global temperature 
+   // request to all devices on the bus 
+  /********************************************************************/
+   //Serial.print(" Requesting temperatures..."); 
+   bodyTempSensor.requestTemperatures(); // Send the command to get temperature readings 
+   //Serial.println("DONE"); 
+  /********************************************************************/
+   Serial.print("Temperature is: ");
+   bodyTemp = bodyTempSensor.getTempCByIndex(0);
+   strTemp = String(bodyTemp);
+   Serial.print(bodyTemp);
+   
+}
+
+void readMPU6050(){
+  mpu6050.update();
+
+  if(mpu6050Timer.checkfinish()){
+    
+    Serial.println("=======================================================");
+    //Serial.print("temp : ");Serial.println(mpu6050.getTemp());
+    acc[0] = (int)(mpu6050.getAccX()*9.81);
+    acc[1] = (int)(mpu6050.getAccY()*9.81);
+    acc[2] = (int)(mpu6050.getAccZ()*9.81);
+    Serial.print("accX : ");Serial.print(acc[0]);
+    Serial.print("\taccY : ");Serial.print(acc[1]);
+    Serial.print("\taccZ : ");Serial.println(acc[2]);
+  
+    //Serial.print("gyroX : ");Serial.print(mpu6050.getGyroX());
+    //Serial.print("\tgyroY : ");Serial.print(mpu6050.getGyroY());
+    //Serial.print("\tgyroZ : ");Serial.println(mpu6050.getGyroZ());
+  
+    //Serial.print("accAngleX : ");Serial.print(mpu6050.getAccAngleX());
+    //Serial.print("\taccAngleY : ");Serial.println(mpu6050.getAccAngleY());
+
+    gyr[0] = (int)mpu6050.getGyroAngleX();
+    gyr[1] = (int)mpu6050.getGyroAngleY();
+    gyr[2] = (int)mpu6050.getGyroAngleZ();
+  
+    Serial.print("gyroAngleX : ");Serial.print(gyr[0]);
+    Serial.print("\tgyroAngleY : ");Serial.print(gyr[1]);
+    Serial.print("\tgyroAngleZ : ");Serial.println(gyr[2]);
+    
+    //Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
+    //Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
+    //Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
+    Serial.println("=======================================================\n");
+
+    strGyr = "(" + String(gyr[0]) + "," + String(gyr[1]) + "," + String(gyr[2]) + ")";
+    strAcc = "(" + String(acc[0]) + "," + String(acc[1]) + "," + String(acc[2]) + ")";
+    
+    mpu6050Timer.resettimer();
+    mpu6050Timer.starttimer();
+  }
 }
 
 void loop()
 {
-  Serial.println("test");
   /*
+  if (tempTimer.checkfinish()){
+    readTemperature();
+    tempTimer.resettimer();
+    tempTimer.starttimer();
+  }
+  
   long irValue = particleSensor.getIR();
-
   if (checkForBeat(irValue) == true)
   {
     //We sensed a beat!
@@ -261,18 +520,28 @@ void loop()
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
-  }
-
+  }  
+  
   Serial.print("IR=");
   Serial.print(irValue);
   Serial.print(", BPM=");
   Serial.print(beatsPerMinute);
   Serial.print(", Avg BPM=");
   Serial.print(beatAvg);
-
-  if (irValue < 50000)
-    Serial.print(" No finger?");
-
+  
+  
+  if (irValue < 50000){
+  //  Serial.print(" No finger?");
+    strBeatAvg = "No finger";
+  }
+  else {
+    strBeatAvg = String(beatAvg);
+  }  
+  
   Serial.println();
+
+  readMPU6050();
+
+  oledDisplayLoop();
   */
 }
