e5ef790acfba7ccca25a0df6b581cc452b645fb6
eddy <eddylau0328@gmail.com>
2020-02-14 12:16:01 +0800

    update references_list and learning keras

--- Machine_Learning/program/smartwatch_learning.py
+++ Machine_Learning/program/smartwatch_learning.py
@@ -1,7 +1,7 @@
 import json
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPool1D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D
 
 
 
@@ -15,5 +15,7 @@ dict_list = get_data("smartwatch_data/work_acc.json", "acc")
 data = [[dict_obj["acc_x"], dict_obj["acc_y"], dict_obj["acc_z"]] for dict_obj in dict_list]
 
 
-#model = Sequential()
-#model.add(Conv1D(30, 3, activation='relu', input_shape=(,)))
+model = Sequential()
+model.add(Conv1D(filters = 64, kernel_size = 5, activation='relu', input_shape=(30,3), padding='same'))
+model.add(MaxPooling1D(pool_size = 5, strides=5, padding='valid'))
+model.summary()
