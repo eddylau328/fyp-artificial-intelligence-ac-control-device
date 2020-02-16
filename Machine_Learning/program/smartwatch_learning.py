24b52d3803e504cc9371b2c43c2b5dc347e64759
eddy <eddylau0328@gmail.com>
2020-02-16 23:52:06 +0800

    Modified smartwatch_learning.py

--- Machine_Learning/program/smartwatch_learning.py
+++ Machine_Learning/program/smartwatch_learning.py
@@ -1,10 +1,18 @@
 import json
+from enum import Enum
 from random import shuffle
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D
+import sys
+sys.setrecursionlimit(1500)
 
+class MovementType(Enum):
+    work = 0
+    rest = 1
+    sleep = 2
+    move = 3
 
 
 def get_data(path, dataname):
@@ -21,33 +29,45 @@ def process_data(data):
     return data_list
 
 
-def copy_list(dict_list, size, overlap_size):
+def copy_list(dict_list, size, cover_size):
+
+    counter = 0
+    copy_dict_list = []
+    while ((counter+size) <= len(dict_list)):
+        for obj in dict_list[counter:counter+size]:
+            copy_dict_list.append(obj)
+        counter += cover_size
+
     iterate = 1
     acc_x, acc_y, acc_z = [], [], []
     x = []
-    for j in range(len(dict_list)):
-        dict_obj = dict_list[j]
+
+    for j in range(len(copy_dict_list)):
+        dict_obj = copy_dict_list[j]
         acc_x.append(dict_obj['acc_x'])
         acc_y.append(dict_obj['acc_y'])
         acc_z.append(dict_obj['acc_z'])
-        if (iterate == overlap_size):
-            x = copy_list(dict_list[j+1:], size, overlap_size)
         if (iterate == size):
-            if x is not []:
-                x.append([acc_x,acc_y,acc_z])
-            else:
-                x = [[acc_x,acc_y,acc_z],]
-            return x
-
-        iterate += 1
+            x.append([acc_x,acc_y,acc_z])
+            acc_x, acc_y, acc_z = [], [], []
+            iterate = 1
+        else:
+            iterate += 1
 
     return x
 
 
-def transform_2_train_data(dict_list, size, y_value, x_train, y_train, overlap=False, overlap_size=1):
-    if (overlap == False):
-        overlap_size = size
-    x = copy_list(dict_list, size, overlap_size)
+def transform_2_train_data(dict_list, size, y_value, x_train, y_train, overlap, overlap_size):
+    if(overlap == False):
+        cover_size = size
+    else:
+        if (overlap_size >= 0 and overlap_size < size):
+            cover_size = size - overlap_size
+        else:
+            print("Error in transform_2_train_data")
+            return
+
+    x = copy_list(dict_list, size, cover_size)
     y = [y_value for i in range(len(x))]
 
     for data in x:
@@ -57,48 +77,74 @@ def transform_2_train_data(dict_list, size, y_value, x_train, y_train, overlap=F
 
     return x_train, y_train
 
+
+
+MOVE_DATA_SIZE = 1
+WORK_DATA_SIZE = 2
 SLEEP_DATA_SIZE = 2
 REST_DATA_SIZE = 2
-#move_acc = get_data("smartwatch_data/move_acc.json", "acc")
-#move_acc = process_data(move_acc)
 
-work_acc = get_data("smartwatch_data/work_acc_4hz.json", "acc")
-work_acc = process_data(work_acc)
+PERIOD_SIZE = 20    # 4Hz => 4Hz * sec = PERIOD_SIZE
+OVERLAP_DATA = False
+OVERLAP_SIZE = 0
+
+move_acc = []
+for i in range(MOVE_DATA_SIZE):
+    move_acc.append(process_data(get_data("smartwatch_data/move_acc_4hz_"+str(i)+".json", "acc")))
+    print("Size of move_acc data "+str(i) + " is %s" %(len(move_acc[i])))
+
+
+work_acc = []
+for i in range(WORK_DATA_SIZE):
+    work_acc.append(process_data(get_data("smartwatch_data/work_acc_4hz_"+str(i)+".json", "acc")))
+    print("Size of work_acc data "+str(i) + " is %s" %(len(work_acc[i])))
 
 sleep_acc = []
 for i in range(SLEEP_DATA_SIZE):
     sleep_acc.append(process_data(get_data("smartwatch_data/sleep_acc_4hz_"+str(i)+".json", "acc")))
+    print("Size of sleep_acc data "+str(i) + " is %s" %(len(sleep_acc[i])))
 
 rest_acc = []
 for i in range(SLEEP_DATA_SIZE):
     rest_acc.append(process_data(get_data("smartwatch_data/rest_acc_4hz_"+str(i)+".json", "acc")))
+    print("Size of rest_acc data "+str(i) + " is %s" %(len(rest_acc[i])))
+
 
 x_train, y_train = [], []
-x_train, y_train = transform_2_train_data(work_acc, 40, 0, x_train, y_train, overlap=True, overlap_size=40)
-print("Size of work_acc data is %s" %len(x_train))
-tmp = len(x_train)
+tmp = 0
 
+for i in range(MOVE_DATA_SIZE):
+    x_train, y_train = transform_2_train_data(move_acc[i], PERIOD_SIZE, MovementType.move.value, x_train, y_train, overlap=OVERLAP_DATA, overlap_size=OVERLAP_SIZE)
+    print("Number of period of move_acc data "+str(i) + " is %s" %(len(x_train)-tmp))
+    tmp = len(x_train)
 
+for i in range(WORK_DATA_SIZE):
+    x_train, y_train = transform_2_train_data(work_acc[i], PERIOD_SIZE, MovementType.work.value, x_train, y_train, overlap=OVERLAP_DATA, overlap_size=OVERLAP_SIZE)
+    print("Number of period of work_acc data "+str(i) + " is %s" %(len(x_train)-tmp))
+    tmp = len(x_train)
 
 for i in range(SLEEP_DATA_SIZE):
-    x_train, y_train = transform_2_train_data(sleep_acc[i], 40, 1, x_train, y_train, overlap=True, overlap_size=40)
-    print("Size of sleep_acc data "+str(i) + " is %s" %(len(x_train)-tmp))
+    x_train, y_train = transform_2_train_data(sleep_acc[i], PERIOD_SIZE, MovementType.sleep.value, x_train, y_train, overlap=OVERLAP_DATA, overlap_size=OVERLAP_SIZE)
+    print("Number of period of sleep_acc data "+str(i) + " is %s" %(len(x_train)-tmp))
     tmp = len(x_train)
 
 for i in range(REST_DATA_SIZE):
-    x_train, y_train = transform_2_train_data(rest_acc[i], 40, 2, x_train, y_train, overlap=True, overlap_size=40)
-    print("Size of rest_acc data " +str(i) + " is %s" %(len(x_train)-tmp))
+    x_train, y_train = transform_2_train_data(rest_acc[i], PERIOD_SIZE, MovementType.rest.value, x_train, y_train, overlap=OVERLAP_DATA, overlap_size=OVERLAP_SIZE)
+    print("Number of period of rest_acc data " +str(i) + " is %s" %(len(x_train)-tmp))
     tmp = len(x_train)
 
 TOTAL_DATA_SIZE = len(x_train)
+print("The total data size is %s" %TOTAL_DATA_SIZE)
 
 for i in range(len(y_train)):
     if y_train[i] is 0:
-        y_train[i] = [1,0,0]
+        y_train[i] = [1,0,0,0]
     elif y_train[i] is 1:
-        y_train[i] = [0,1,0]
+        y_train[i] = [0,1,0,0]
     elif y_train[i] is 2:
-        y_train[i] = [0,0,1]
+        y_train[i] = [0,0,1,0]
+    elif y_train[i] is 3:
+        y_train[i] = [0,0,0,1]
 
 
 combine = list(zip(x_train, y_train))
@@ -106,20 +152,20 @@ shuffle(combine)
 x_train, y_train = zip(*combine)
 x_train = np.asarray(x_train)
 y_train = np.asarray(y_train)
-x_train = x_train.reshape(TOTAL_DATA_SIZE,40,3)
-y_train = y_train.reshape(TOTAL_DATA_SIZE,3)
+x_train = x_train.reshape(TOTAL_DATA_SIZE,PERIOD_SIZE,3)
+y_train = y_train.reshape(TOTAL_DATA_SIZE,4)
 print(np.shape(x_train))
 print(np.shape(y_train))
 
 
 model = Sequential([
-        Conv1D(filters = 80, kernel_size = 5, activation='relu', input_shape=(40,3), padding='same'),
+        Conv1D(filters = 64, kernel_size = 5, activation='relu', input_shape=(PERIOD_SIZE,3), padding='same'),
         MaxPooling1D(pool_size = 4, strides=4, padding='valid'),
-        Conv1D(filters = 120, kernel_size = 2, activation='relu', padding='same'),
+        Conv1D(filters = 128, kernel_size = 2, activation='relu', padding='same'),
         MaxPooling1D(pool_size = 2, strides=2, padding='valid'),
         Flatten(),
         Dense(64, activation='relu'),
-        Dense(3, activation='softmax'),
+        Dense(4, activation='softmax'),
     ])
 
 model.summary()
