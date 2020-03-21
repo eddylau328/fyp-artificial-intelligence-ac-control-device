e5b266fa1ca37681909357e41397803fb8d8eda4
eddy <eddylau0328@gmail.com>
2020-03-22 00:33:15 +0800

    added ac_env script to create env for machine learning

---
+++ Machine_Learning/program/ac_env.py
@@ -0,0 +1,8 @@
+import gym
+from gym import spaces
+
+class AC_Env(gym.Env):
+
+    metadata = {'render.modes' : ['human']}
+
+    def __init__(self)
--- Machine_Learning/program/ac_firebase_remote.py
+++ Machine_Learning/program/ac_firebase_remote.py
@@ -30,6 +30,8 @@ class AC_remote:
 if (__name__ == '__main__'):
     r = rt.Realtime_firebase()
     remote = AC_remote("fyp0001")
-    if (r.set("/Devices/fyp0001","receive_action",{"command":"ir power off", "is_new_action": True})):
-        print("Sent command to firebase")
-
+    command = ""
+    while (command != "quit"):
+        command = input()
+        if (r.set("/Devices/fyp0001","receive_action",{"command":command, "is_new_action": True})):
+            print("Sent command to firebase")
