91bd0f4a0400b0f314733ab02ea8fd9d0ca95f05
eddy <eddylau0328@gmail.com>
2020-03-22 12:07:07 +0800

    mobile app control fragment add switch button

--- Mobile_app/FYP_AC_Monitor/app/src/main/java/com/example/fyp_ac_monitor/ControlFragment.java
+++ Mobile_app/FYP_AC_Monitor/app/src/main/java/com/example/fyp_ac_monitor/ControlFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +14,24 @@ import androidx.fragment.app.Fragment;
 
 public class ControlFragment extends Fragment {
 
+    Switch _powerSwitch;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_control, container, false);
+        View show_view = inflater.inflate(R.layout.fragment_control, container, false);
+        _powerSwitch = (Switch) show_view.findViewById(R.id.control_fragment_power_switch);
+        _powerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true){
+                    Toast.makeText(getActivity(), "ON", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getActivity(), "OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return show_view;
     }
 }
