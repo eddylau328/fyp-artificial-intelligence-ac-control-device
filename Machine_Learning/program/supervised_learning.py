fc72739f0f593de8f523fe26c6560412c9f99d2b
eddy <eddylau0328@gmail.com>
2020-04-10 16:39:39 +0800

    added basic keras sequential model

--- Machine_Learning/program/supervised_learning.py
+++ Machine_Learning/program/supervised_learning.py
@@ -1,10 +1,10 @@
-import enum
-from tensorflow.keras.model import Model
-from tensorflow.keras.layers import Input, Dense
+from enum import Enum
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
 
-class Feedback(enum.Enum):
+class Feedback(Enum):
     very_hot = 0
     hot = 1
     a_bit_hot = 2
@@ -13,6 +13,7 @@ class Feedback(enum.Enum):
     a_bit_cold = 5
     cold = 6
     very_cold = 7
+
 
 ##############################################################
 #
@@ -27,32 +28,30 @@ class Feedback(enum.Enum):
 #
 ##############################################################
 
-
-def SupervisedLearningModel(input_shape, output_shape, model_name, learning_rate ):
-    X_input = Input(input_shape)
-
-    # 'Dense' is the basic form of a neural network layer
-    # Input Layer of state size and Hidden Layer with 512 nodes
-    X = Dense(512, input_shape=input_shape, activation="relu", kernel_initializer='he_uniform')(X_input)
-
-    # Hidden layer with 256 nodes
-    X = Dense(256, activation="relu", kernel_initializer='he_uniform')(X)
-
-    # Hidden layer with 64 nodes
-    X = Dense(64, activation="relu", kernel_initializer='he_uniform')(X)
-
-    # Output Layer with # of actions: 2 nodes (left, right)
-    X = Dense(action_space, activation="linear", kernel_initializer='he_uniform')(X)
-
-    model = Model(inputs = X_input, outputs = X, name=model_name)
-    model.compile(loss="mse", optimizer=RMSprop(lr=learning_rate, rho=0.95, epsilon=0.01), metrics=["accuracy"])
-
-    model.summary()
-    return model
+parameters = {
+    'input_shape':17,
+    'output_shape': len(Feedback),
+    'model_name': "Supervised Learning"
+}
 
 
 class SupervisedLearning:
 
-    def __init__(self):
-        self.input_size =
-        self.model = SupervisedLearningModel(input_shape=(self.input_size,), output_shape = ,)
+    def __init__(self, **kwargs):
+        self.input_shape = kwargs['input_shape']
+        self.output_shape = kwargs['output_shape']
+        self.model_name = kwargs['model_name']
+        self.initiate_model()
+
+    def initiate_model(self):
+        self.model = Sequential(name=self.model_name)
+        self.model.add(Dense(32, input_shape=(self.input_shape,), activation='relu'))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(128, activation='relu'))
+        self.model.add(Dense(self.output_shape, activation='softmax'))
+        self.model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
+        self.model.summary()
+
+
+if (__name__ == '__main__'):
+    model = SupervisedLearning(**parameters)
